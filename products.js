d8f660c1901ebd55815326a6b5e4e66678b904d2
unknown <medellincode@gmail.com>
2024-07-22 14:56:21 -0500

    Configuración inicial del proyecto

---
+++ products.js
@@ -0,0 +1,36 @@
+const products = [
+
+
+  {
+    id: 4,
+    name: "Memoria RAM 16GB DDR4",
+    brand: "TechMemory",
+    desc: "Memoria RAM de 16GB DDR4 para alto rendimiento",
+    price: 180000,
+    image: "https://res.cloudinary.com/dn7rno9on/image/upload/v1721159595/mejorcomputo/pndslvtfrxybxm5zmc04.jpg",
+},
+
+{
+    id: 6,
+    name: "Disco Duro SSD 1TB",
+    brand: "FastStorage",
+    desc: "Disco duro SSD de 1TB para almacenamiento rápido y seguro",
+    price: 200000,
+    image: "https://res.cloudinary.com/dn7rno9on/image/upload/v1721159595/mejorcomputo/pcvvlbkhnhwk9zzdqupy.jpg",
+},
+
+{
+    id: 13,
+    name: "Monitor 27\" 4K UHD",
+    brand: "UltraVision",
+    desc: "Pantalla de 27 pulgadas con resolución 4K UHD para una experiencia visual increíble",
+    price: 250000,
+    image: "https://res.cloudinary.com/dn7rno9on/image/upload/v1721159595/mejorcomputo/jnsa5um3kdnwdkd0ocsv.jpg",
+},
+
+
+
+
+];
+
+module.exports = products;
