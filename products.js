aa2cf6cb2d05cd6ee33ee56c82df0eb368d6974b
amky77 <oficina.akmdigital@gmail.com>
2024-10-09 15:44:14 -0500

    Add files via upload

--- products.js
+++ products.js
@@ -1,36 +1,68 @@
 const products = [
-
-
-  {
-    id: 4,
-    name: "Memoria RAM 16GB DDR4",
-    brand: "TechMemory",
-    desc: "Memoria RAM de 16GB DDR4 para alto rendimiento",
-    price: 180000,
-    image: "https://res.cloudinary.com/dn7rno9on/image/upload/v1721159595/mejorcomputo/pndslvtfrxybxm5zmc04.jpg",
-},
-
-{
-    id: 6,
-    name: "Disco Duro SSD 1TB",
-    brand: "FastStorage",
-    desc: "Disco duro SSD de 1TB para almacenamiento rápido y seguro",
-    price: 200000,
-    image: "https://res.cloudinary.com/dn7rno9on/image/upload/v1721159595/mejorcomputo/pcvvlbkhnhwk9zzdqupy.jpg",
-},
-
-{
-    id: 13,
-    name: "Monitor 27\" 4K UHD",
-    brand: "UltraVision",
-    desc: "Pantalla de 27 pulgadas con resolución 4K UHD para una experiencia visual increíble",
-    price: 250000,
-    image: "https://res.cloudinary.com/dn7rno9on/image/upload/v1721159595/mejorcomputo/jnsa5um3kdnwdkd0ocsv.jpg",
-},
-
-
-
-
-];
-
-module.exports = products;
+    {
+      id: 1,
+      name: "ACER A314-23P-R2KX",
+      brand: "ACER",
+      desc: "AMD RYZEN 5 7520U - 8GB LPDDR5 - 512GB SSD - PANTALLA 14\" HD - NO DVD - FREEDOS - PURE SILVER",
+      price: 1600000,
+    },
+    {
+      id: 2,
+      name: "ACER A315-24PT-R6GZ",
+      brand: "ACER",
+      desc: "AMD RYZEN 5 7520U - 8GB LPDDR5 - 512GB SSD - PANTALLA 15.6\" FHD (TOUCH) - NO DVD - FREEDOS - PURE SILVER",
+      price: 1800000,
+    },
+    {
+      id: 3,
+      name: "LENOVO 3 14ALC6",
+      brand: "LENOVO",
+      desc: "AMD RYZEN 7 5700U - 20GB DDR4 (EXPANDIDO) - 512GB SSD - PANTALLA 14\" FHD - NO DVD - HDMI - FREEDOS - ABYSS BLUE",
+      price: 1990000,
+    },
+    {
+      id: 4,
+      name: "HP 255 G10",
+      brand: "HP",
+      desc: "AMD RYZEN 5 7520U - 8GB DDR4 - 512GB SSD - PANTALLA 15.6\" - NO DVD - HDMI - FREEDOS - NEGRO",
+      price: 1550000,
+    },
+    {
+      id: 5,
+      name: "HP 15-EF2517LA",
+      brand: "HP",
+      desc: "AMD RYZEN 5 5500U - 12GB DDR4 - 512GB SSD - PANTALLA 15.6\" HD - NO DVD - FREEDOS - AZUL INDIGO",
+      price: 1690000,
+    },
+    {
+      id: 6,
+      name: "HP PAVILION 15-EH3003LA",
+      brand: "HP",
+      desc: "AMD RYZEN 7 7730U - 16GB DDR4 - 512GB SSD - PANTALLA 15.6\" FHD - NO DVD - FREEDOS - AZUL NIEBLA",
+      price: 2300000,
+    },
+    {
+      id: 7,
+      name: "DELL INSPIRON 15 3515",
+      brand: "DELL",
+      desc: "AMD RYZEN 7 3700U - 8GB DDR4 - 512GB SSD - PANTALLA 15.6\" FHD - NO DVD - HDMI - WIN 11 - MIST BLUE SPECKLE",
+      price: 1900000,
+    },
+    {
+      id: 8,
+      name: "DELL INSPIRON 15 3515",
+      brand: "DELL",
+      desc: "AMD RYZEN 7 3700U - 8GB DDR4 - 512GB SSD - PANTALLA 15.6\" FHD - NO DVD - HDMI - WIN 11 - MIST BLUE SPECKLE",
+      price: 2250000,
+    },
+    {
+      id: 9,
+      name: "ASUS M3604YA-MB281",
+      brand: "ASUS",
+      desc: "AMD RYZEN 7 7730U - 16GB DDR4 - 1 TERA SSD - PANTALLA 16.0\" WUXGA - NO DVD - HDMI - ENDLESS - COOL SILVER",
+      price: 2600000,
+    },
+  ];
+  
+  module.exports = products;
+  
